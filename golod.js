361702dd7c245cbc188354a3958bf174eb6bead5
GulGolod <151571206+GulGolod@users.noreply.github.com>
2024-03-14 15:52:25 +0100

    golod2

---
+++ golod.js
@@ -0,0 +1,13 @@
+<h1>Player 1: <span id="player1-hunger"></span> / <span id="player1-max-hunger">200</span></h1>
+<h1>Player 2: <span id="player2-hunger"></span> / <span id="player2-max-hunger">180</span></h1>
+
+<script>
+  class Player {
+    constructor(name, maxHealth, maxHunger) {
+      this.name = name;
+      this.health = maxHealth;
+      this.hunger = maxHunger;
+    }
+  }
+
+  let player1 = new Player("Player 1", 100, 200);
